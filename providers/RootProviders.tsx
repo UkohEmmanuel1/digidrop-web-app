06817328a128131ac5ec1f7f8607f1ff54c36d9f
henry <aengrhenry@gmail.com>
2025-12-28 00:37:41 +0100

    fix rainbowkit style

--- providers/RootProviders.tsx
+++ providers/RootProviders.tsx
@@ -1,6 +1,6 @@
 'use client';
-
-
+import React from "react"
+import '@rainbow-me/rainbowkit/styles.css';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { WagmiProvider } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
